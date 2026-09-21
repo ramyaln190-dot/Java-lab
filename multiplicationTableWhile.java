fd53c3373f2e5223115b49cf5d106c0d8ee1c21b
ramyaln190-dot <ramyaln190@gmail.com>
2026-09-21 18:47:46 +0530

    Add files via upload

---
+++ multiplicationTableWhile.java
@@ -0,0 +1,11 @@
+public class multiplicationTableWhile {
+    public static void main(String[] args) {
+        int number = 8;
+        int i = 1;
+
+        while (i <= 10) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+            i++; // Increment the counter
+        }
+    }
+}
