f8a680bffc0076abf7cbdb6cd56d4cce0317795d
ramyaln190-dot <ramyaln190@gmail.com>
2026-09-21 17:16:21 +0530

    Add files via upload

---
+++ TypeCasting.java
@@ -0,0 +1,11 @@
+public class TypeCasting {
+    public static void main(String[] args) {
+
+        // Double to integer
+        double a = 10.5;
+        int b = (int) a;
+
+        System.out.println("Double value: " + a);
+        System.out.println("Integer value: " + b);
+    }
+}
