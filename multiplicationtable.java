0f6beed8b8e2cee0bf7c3b8c35d8e4e9dca6b481
ramyaln190-dot <ramyaln190@gmail.com>
2026-09-21 18:44:21 +0530

    Add files via upload

---
+++ multiplicationtable.java
@@ -0,0 +1,11 @@
+public class multiplicationtable {
+    public static void main(String[] args) {
+        int number = 8;
+
+        // Loop from 1 to 10
+        for (int i = 1; i <= 10; i++) {
+            // Print the multiplication format
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+    }
+}
