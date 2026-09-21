e8a22ef852e897f9b9fe075493f79e5ff534ac90
ramyaln190-dot <ramyaln190@gmail.com>
2026-09-21 18:58:28 +0530

    Add files via upload

---
+++ SumOfDigits.java
@@ -0,0 +1,13 @@
+public class SumOfDigits {
+    public static void main(String[] args) {
+        int n = 7584;
+        int sum = 0;
+
+        while (n > 0) {
+            sum = sum + (n % 10);
+            n = n / 10;
+        }
+
+        System.out.println("Sum of digits = " + sum);
+    }
+}
